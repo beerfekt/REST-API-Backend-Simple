2fbd6fc88d195d1a79b8a0f81f9bc4298ecfb602
beerfekt <christoph.beer@gmx.de>
2019-01-03 11:17:31 +0100

    added test request examples to each method

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # REST-api-Symfony4-AngularJS
-A minimal Example of a REST-Api based on Symfony4-FOS-Rest-Bundle and AngularJS as Frontend
+A minimal Example of a REST-Api based on Symfony4-FOS-Rest-Bundle and AngularJS as Frontend  
+
+**Architecture:**  
+  
+  
+* Backend
+  *Symfony4
+
+*Frontend
+  *AngularJS
+
+*Communication
+  *JSON
 
 
-
-## 1. add an .env file (Read more at the Official Symfony-documentation)
-## 2. integrate the project into a web enviroment (Vagrant Homestead Box for Example)
+1. clone the project
+2. add an .env file (Read more at the Official Symfony-documentation)
+3. integrate the project into a web enviroment (Vagrant Homestead Box for Example)
+4. 
--- src/Controller/Rest/ArticleController.php
+++ src/Controller/Rest/ArticleController.php
@@ -14,6 +14,10 @@ use FOS\RestBundle\View\View;
 class ArticleController extends FOSRestController {
 
 
+
+
+    // Test via terminal:
+    // curl -i -X PUT -H 'Content-Type: application/json' -d '{"title": "Updated Title", "content": "updated Content"}' http://rest-tutorial.test/api/articles
     /**
      * Creates an Article resource
      * @Rest\Post("/articles")
@@ -28,19 +32,19 @@ class ArticleController extends FOSRestController {
         $article->setTitle($request->get('title'));
         $article->setContent($request->get('content'));
 
-        //In DB schreiben:
+    //In DB schreiben:
         $entityManager = $this->getDoctrine()->getManager();
-
         // tell Doctrine you want to (eventually) save the Product (no queries yet)
         $entityManager->persist($article);
-
         // actually executes the queries (i.e. the INSERT query)
         $entityManager->flush();
-
         // In case our POST was a success we need to return a 201 HTTP CREATED response
         return View::create($article, Response::HTTP_CREATED);
     }
 
+
+    //Test getArticles with Browser-URL:
+    // http://rest-tutorial.test/api/articles/3
 
     /**
      * Retrieves an Article resource
@@ -54,6 +58,9 @@ class ArticleController extends FOSRestController {
     }
 
 
+    //Test getArticles with Browser-URL:
+    // http://rest-tutorial.test/api/articles
+
     /**
      * Retrieves a collection of Article resource
      * @Rest\Get("/articles")
@@ -66,30 +73,51 @@ class ArticleController extends FOSRestController {
     }
 
 
+    // Test - via Terminal:
+    // curl -i -X PUT -H 'Content-Type: application/json' -d '{"title": "Updated Title", "content": "updated Content"}' http://rest-tutorial.test/api/articles/3
     /**
      * Replaces Article resource
      * @Rest\Put("/articles/{articleId}")
      */
     public function putArticle(int $articleId, Request $request): View
     {
-
         $article = $this->getDoctrine()->getRepository(Article::class)->find($articleId);
-
         if ($article) {
             $article->setTitle($request->get('title'));
             $article->setContent($request->get('content'));
-
-            //In DB schreiben:
+        //In DB schreiben:
             $entityManager = $this->getDoctrine()->getManager();
             // tell Doctrine you want to (eventually) save the Product (no queries yet)
             $entityManager->persist($article);
             // actually executes the queries (i.e. the INSERT query)
             $entityManager->flush();
         }
-
         // In case our PUT was a success we need to return a 200 HTTP OK response with the object as a result of PUT
         return View::create($article, Response::HTTP_OK);
     }
 
+
+    //Test Delete -> Terminal:
+    // curl -i -X DELETE http://rest-tutorial.test/api/articles/3
+    /**
+     * Deletes Article resource
+     * @Rest\Delete("/articles/{articleId}")
+     */
+    public function deleteArticle(int $articleId): View
+    {
+        //grep the article of db
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($articleId);
+        //if there's a article token...
+        if ($article) {
+            //fetch the "doctrine db-manager"
+            $entityManager = $this->getDoctrine()->getManager();
+            // tell Doctrine you want to delete the article
+            $entityManager->remove($article);
+            // actually executes the delete query
+            $entityManager->flush();
+        }
+        // In case our DELETE was a success we need to return a 204 HTTP NO_CONTENT response with the object as a result of PUT
+        return View::create($article, Response::HTTP_NO_CONTENT);
+    }
 
 }
